b050e3edf23ff12de8093164f134a12adb4848fc
3Vivi <viviannelouize91@gmail.com>
2026-05-06 17:24:09 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,56 +1,67 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(name_)
 
-@app.route('/index')
+@app.route('/')
 def index():
-    return render_template('index.html')
+    return 'Essa é minha primeira aplicação em Flask!'
 
-@app.route('/contato')
-def contato():
-    nome = "Gaby"
-    return render_template('index.html', title= 'Página Inicial', usuario=usuario, nome=None)
+# Crie uma rota /login no seu projeto flask que carregue uma página HTML (login. html, por exemplo) contendo um formulário com 2 campos de texto: um para o nome do usuário e outro para senha.
 
-@app.route()
-def usuario():
-    nome = {'nome','Gaby','email','cosrtezin@gmail.com'}
-    return render_template('index.html', title= 'Página Inicial', nome=nome)
+@app.route('/login')
+def login():
+    return render_template('login.html')
+# Crie uma rota/alunos que renderize uma página HTML (alunos. html, por exemplo) com o nome e a matrícula de alguns alunos em uma tabela.
 
-@app.route('/dados', defaults={"nome":"usuário comum"})
-@app.route('/dados/<nome>')
-def dados(nome):
-    return f'Olá,{nome}!'
+@app. route('/alunos')
+def alunos():
+    lista_alunos = [
+        {'nome': 'Alice', 'matricula': '12345678'}
+        {'nome': 'Bruno', 'matricula': '98765432'},
+        {'nome': 'Clara', 'matricula': '45678912'},
+        {'nome': 'Marcos','matricula': '74125896'},
+        {'nome': 'Valéria', 'matricula': '85236974'}
+   ]
 
-@app.route('/semestre/<int: x>')
-def semestre(x):
-    return 'Estamos no semestre' + str(x)
+    return render_template(' alunos. html', alunos-lista_alunos)
 
-@app.route('/pagamento/<float:valor>')
-def pagamento(valor):
-    return 'Você pagou: '+ str(valor)
-   
-#Questôes
-
-@app.route("/arearestrita/<id>")
-def area(id):
-    cadeados = {
-        "1": "Cadeado Fechado",
-        "2": "Cadeado Aberto"
-    }
-
-    return cadeados.get(id, "ID inválido")
+#Crie uma rota /arearestrita que recebe um parâmetro (ex: / arearestrita/
 
 
-@app.route("/operacao/<tipo>/<int:op1>/<int:op2>")
-def operacao(tipo, op1, op2):
-    operacoes = {
-        "sum": op1 + op2,
-        "sub": op1 - op2,
-        "mult": op1 * op2,
-        "div": op1 / op2
-    }
 
-    return f"Resultado: {operacoes.get(tipo, 'Operação inválida')}"
+@app.route("/arearestrita/<int:id>")
+def arearestrita(id):
+    if id == 1:
+        return 'cadeado Fechado'
+    elif id == 2:
+          return 'cadeado Aberto'
+    else:
+        return "Acesso inválido"
+    return render_template(' restrita html', imagem=imagem)
 
-if __name__ == '__main__':
-    app.run()
+# Crie uma rota /operacao que recebe 3 parâmetros (ex: /operacao/<tipo>/<op1>/<op2>.
+
+def operacao (tipo, op1, op2):
+             return "Erro: divisâo por zero"
+        resultado = op1 / op2
+    else:
+        return "Tipo de operação inválido"
+
+    return f"Resultado: {resultado}"
+
+
+@app.route('/somar', defaults={"1": "0", "n2": "0"})
+@app. route('/somar/<int:n2>')
+def somar (n1, n2):
+
+        resultado = n1 + n2
+        return str (resultado)
+
+@app.route('/soma', defaults={"n1": "0", "n2": "0"})
+@app. route('/soma/<int:n1>/<int:n2>')
+def soma(n1, n2):
+  resultado = n1 + n2
+  return render_template('somar. html', n1=n1, n2=n2,
+  resultado=resultado)
+if _name_ == '_main_':
+    app. run(debug=True)
