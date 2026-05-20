069651d29bf8c1c880ad665de7999ed1d5473c85
3Vivi <viviannelouize91@gmail.com>
2026-05-20 17:09:55 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,67 +1,79 @@
 from flask import Flask, render_template
 
-app = Flask(name_)
+app = Flask(__name__)
 
-@app.route('/')
+# Página inicial
+@app.route("/")
 def index():
-    return 'Essa é minha primeira aplicação em Flask!'
-
-# Crie uma rota /login no seu projeto flask que carregue uma página HTML (login. html, por exemplo) contendo um formulário com 2 campos de texto: um para o nome do usuário e outro para senha.
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-# Crie uma rota/alunos que renderize uma página HTML (alunos. html, por exemplo) com o nome e a matrícula de alguns alunos em uma tabela.
-
-@app. route('/alunos')
-def alunos():
-    lista_alunos = [
-        {'nome': 'Alice', 'matricula': '12345678'}
-        {'nome': 'Bruno', 'matricula': '98765432'},
-        {'nome': 'Clara', 'matricula': '45678912'},
-        {'nome': 'Marcos','matricula': '74125896'},
-        {'nome': 'Valéria', 'matricula': '85236974'}
-   ]
-
-    return render_template(' alunos. html', alunos-lista_alunos)
-
-#Crie uma rota /arearestrita que recebe um parâmetro (ex: / arearestrita/
+    return render_template("index.html")
 
 
+# Cardápio
+@app.route("/cardapio")
+def cardapio():
+    return render_template("cardapio.html")
 
-@app.route("/arearestrita/<int:id>")
-def arearestrita(id):
-    if id == 1:
-        return 'cadeado Fechado'
-    elif id == 2:
-          return 'cadeado Aberto'
+
+# Página dinâmica do lanche
+@app.route("/lanche/<nome>")
+def lanche(nome):
+
+    mensagem = ""
+
+    if nome.lower() == "pizza":
+        mensagem = "Pizza deliciosa saindo do forno!"
+    elif nome.lower() == "sushi":
+        mensagem = "sushi fresquinho!"
+    elif nome.lower() == "batata":
+        mensagem = "Batata frita crocante e saborosa!"
+    elif nome.lower() == "milkshake":
+        mensagem = "Milkshake gelado e cremoso!"
     else:
-        return "Acesso inválido"
-    return render_template(' restrita html', imagem=imagem)
+        mensagem = "Lanche não encontrado."
 
-# Crie uma rota /operacao que recebe 3 parâmetros (ex: /operacao/<tipo>/<op1>/<op2>.
+    return render_template(
+        "lanche.html",
+        nome=nome,
+        mensagem=mensagem
+    )
 
-def operacao (tipo, op1, op2):
-             return "Erro: divisâo por zero"
-        resultado = op1 / op2
+# Página de pedidos
+@app.route("/pedidos")
+def pedidos():
+
+    pedidos = [
+        {"cliente": "Ana", "pedido": "Pizza", "valor": "R$ 35"},
+        {"cliente": "Pedro", "pedido": "Temaki hot", "valor": "R$ 14"},
+        {"cliente": "Carlos", "pedido": "Batata Frita", "valor": "R$ 20"},
+        {"cliente": "Julia", "pedido": "Milkshake", "valor": "R$ 18"}
+    ]
+
+    return render_template(
+        "pedidos.html",
+        pedidos=pedidos
+    )
+
+# Página dinâmica do cliente
+@app.route("/cliente/<nome>/<cidade>")
+def cliente(nome, cidade):
+
+    if cidade.lower() == "natal":
+        status = "Entrega disponível!"
     else:
-        return "Tipo de operação inválido"
+        status = "Entrega indisponível."
 
-    return f"Resultado: {resultado}"
+    return render_template(
+        "cliente.html",
+        nome=nome,
+        cidade=cidade,
+        status=status
+    )
+
+# Página contato
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
 
 
-@app.route('/somar', defaults={"1": "0", "n2": "0"})
-@app. route('/somar/<int:n2>')
-def somar (n1, n2):
-
-        resultado = n1 + n2
-        return str (resultado)
-
-@app.route('/soma', defaults={"n1": "0", "n2": "0"})
-@app. route('/soma/<int:n1>/<int:n2>')
-def soma(n1, n2):
-  resultado = n1 + n2
-  return render_template('somar. html', n1=n1, n2=n2,
-  resultado=resultado)
-if _name_ == '_main_':
-    app. run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
